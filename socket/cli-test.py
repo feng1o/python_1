5d446d2b3173409691bcc5d5a09f0eb96401eae4
feng1o <liufeng_mail@126.com>
2016-07-08 11:30:54 +0800

    socket1

--- socket/cli-test.py
+++ socket/cli-test.py
@@ -13,9 +13,9 @@ s.connect((HOST, PORT))  # 连接socket服务器
 print(socket.gethostname())
 print(socket.gethostbyaddr("127.0.0.1"))
 while True:
-    msg = input("Your msg::")  # 让用户输入消息，去除回车和空格
+    # msg = input("Your msg::")  # 让用户输入消息，去除回车和空格
     # msg = input("Your msg::").strip() #让用户输入消息，去除回车和空格
-    # msg = '0004'
+    msg = '0001'
     if len(msg) < 4:
     	fix = 4 - len(msg)
     	for  x in range(0, fix):
@@ -23,7 +23,8 @@ while True:
     	 	print(msg)
         # continue 
     s.send(msg.encode())  # 向服务器发送消息
-    s.send("a0001fdg".encode())
+    # s.send("a0001fdg".encode())
+    # s.send("abcdefdg".encode())
     if (int)(msg) == 1:
     	print('msg...', 1)
     	s.send("1".encode())   
@@ -32,5 +33,3 @@ while True:
 
     print('Received:', data)
 s.close()
-
-
